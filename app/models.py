1350eb9961fc254edcb11a5f44cffa56a522d658
Plew Woo <pew_woothiphom@hotmail.com>
2022-04-05 18:23:34 +0700

    edit lesson name in videoplayer.html

--- app/models.py
+++ app/models.py
@@ -100,8 +100,8 @@ class MyCourse (models.Model):
     courseId = models.IntegerField(null=True)
     course = models.ForeignKey(AllCourse, on_delete=CASCADE)
     stamp = models.DateTimeField(auto_now_add=True, blank=True, null=True)
-    numVideo = models.IntegerField(default = 0, null=True, blank=True)
-    progress = models.DecimalField(default = 0.0, max_digits=19, decimal_places=2,null=True, blank=True)
+    numVideo = models.IntegerField(default=0, null=True, blank=True)
+    progress = models.DecimalField(default=0, max_digits=19, decimal_places=2,null=True, blank=True)
 
     def __str__(self):
         return '%s (%s)' % (self.user.user.first_name, self.course.courseTitle)
