6a37e429911dffc04f03da22245a8b75e686d592
Plew Woo <pew_woothiphom@hotmail.com>
2022-04-16 10:06:15 +0700

    fixed bugs

--- FinalProject2/settings.py
+++ FinalProject2/settings.py
@@ -161,7 +161,7 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 AWS_ACCESS_KEY_ID = S3Connection(os.environ['AWS_ACCESS_KEY_ID'])
 AWS_SECRET_ACCESS_KEY = S3Connection(os.environ['AWS_SECRET_ACCESS_KEY'])
-AWS_STORAGE_BUCKET_NAME = S3Connection(os.environ['AWS_STORAGE_BUCKET_NAME'])
+AWS_STORAGE_BUCKET_NAME = 'mdtproject-media'
 AWS_QUERYSTRING_AUTH = False
 
 django_heroku.settings(locals())
