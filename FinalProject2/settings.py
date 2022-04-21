89bf0cf8e372501340eba5908a9c39f7aafd1071
Plew Woo <pew_woothiphom@hotmail.com>
2022-04-21 10:25:32 +0700

    delete aws

--- FinalProject2/settings.py
+++ FinalProject2/settings.py
@@ -155,11 +155,4 @@ LOGOUT_REDIRECT_URL = 'signin'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-AWS_QUERYSTRING_AUTH = False
-
 django_heroku.settings(locals())
